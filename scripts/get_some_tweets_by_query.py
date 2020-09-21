f21b3aed276e824d8bb5009f099d19d42ab20740
ZoziLaMalice <lorenzo.camus@essca.eu>
2020-09-21 16:46:05 +0200

    update for my dashboard

--- scripts/get_some_tweets_by_query.py
+++ scripts/get_some_tweets_by_query.py
@@ -5,6 +5,7 @@ import configparser as cp
 from tqdm import tqdm
 import os
 import re
+from datetime import datetime
 
 config = cp.ConfigParser()
 config.read('config.ini')
@@ -21,7 +22,7 @@ auth.set_access_token(access_key, access_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
 
-def get_tweets_by_query(query):
+def get_tweets_by_query(query, nb_tweets):
     # csvFile = open('EconTwitter.csv', 'a')
     # csvWriter = csv.writer(csvFile)
     # query = "#CoronavirusOutbreak" # -filter:retweets"
@@ -32,8 +33,8 @@ def get_tweets_by_query(query):
     #     print(tweet.user.screen_name,' --- ', tweet.user.location,' --- ', tweet.full_text)
 
 
-    date_since = "2019-11-16"
-    number_of_tweets = 1000
+    date_since = datetime.today()
+    number_of_tweets = nb_tweets
 
     # get tweets
     tweets_for_csv = []
@@ -44,7 +45,7 @@ def get_tweets_by_query(query):
     query_to_filename = re.sub('#', '', query)
 
     # write to a new csv file from the array of tweets
-    outfile = os.path.normpath(os.getcwd() + os.sep + os.pardir) + '/comments/' + query_to_filename + "_tweets.csv"
+    outfile = os.path.normpath(os.getcwd()) + '/comments/' + query_to_filename + "_tweets.csv"
     print("writing to " + outfile)
     with open(outfile, 'w', encoding='utf8') as file:
         writer = csv.writer(file, delimiter=',')
@@ -124,4 +125,6 @@ def get_all_tweets_by_query(query):
 # json_limit = api.rate_limit_status()
 # print("Calls restants : " + str(json_limit['resources']['search']['/search/tweets']['remaining']) + '\n')
 
-get_all_tweets_by_query("#CoronavirusOutbreak")
+# get_all_tweets_by_query("#AAPL")
+
+get_tweets_by_query('#AAPL', 1000)
